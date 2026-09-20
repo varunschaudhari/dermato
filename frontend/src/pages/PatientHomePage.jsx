3bffde2802088b4ca46f1bbeaf4cc20be79ac802
varun-cube <varun.chaudhari@cubehighways.com>
2026-09-20 14:19:21 +0530

    fixed link issue

--- frontend/src/pages/PatientHomePage.jsx
+++ frontend/src/pages/PatientHomePage.jsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
+import { Link } from 'react-router-dom'
 import { LineChart, Line, ResponsiveContainer } from 'recharts'
 import {
   ScanFace, TrendingUp, CalendarClock, Gauge, ArrowUpRight, ArrowDownRight,
